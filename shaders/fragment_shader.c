d4cf35e013474ec92d564c77b14d75a3240c8908
Toma Adrian <sirhadrian21@gmail.com>
2023-05-16 23:18:53 +0300

    update

--- shaders/fragment_shader.c
+++ shaders/fragment_shader.c
@@ -19,7 +19,7 @@ uniform vec2 u_mouse;
 #define M u_mouse
 
 #define MAX_MARCHING_STEPS 100.
-#define PRECISION .001
+#define PRECISION .01
 #define MAX_DEPTH 50.
 
 #define PI 3.14159265359
@@ -180,6 +180,97 @@ float opSmoothSubtraction2(float d1, float d2, float k) {
 // Scene
 // =========================================================================================================
 
+// Some useful functions
+vec3 mod289(vec3 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
+vec2 mod289(vec2 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
+vec3 permute(vec3 x) { return mod289(((x * 34.0) + 1.0) * x); }
+
+//
+// Description : GLSL 2D simplex noise function
+//      Author : Ian McEwan, Ashima Arts
+//  Maintainer : ijm
+//     Lastmod : 20110822 (ijm)
+//     License :
+//  Copyright (C) 2011 Ashima Arts. All rights reserved.
+//  Distributed under the MIT License. See LICENSE file.
+//  https://github.com/ashima/webgl-noise
+//
+float snoise(vec2 uv) {
+
+  // Precompute values for skewed triangular grid
+  const vec4 C = vec4(0.211324865405187,
+                      // (3.0-sqrt(3.0))/6.0
+                      0.366025403784439,
+                      // 0.5*(sqrt(3.0)-1.0)
+                      -0.577350269189626,
+                      // -1.0 + 2.0 * C.x
+                      0.024390243902439);
+  // 1.0 / 41.0
+
+  // First corner (x0)
+  vec2 i = floor(uv + dot(uv, C.yy));
+  vec2 x0 = uv - i + dot(i, C.xx);
+
+  // Other two corners (x1, x2)
+  vec2 i1 = vec2(0.0);
+  i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+  vec2 x1 = x0.xy + C.xx - i1;
+  vec2 x2 = x0.xy + C.zz;
+
+  // Do some permutations to avoid
+  // truncation effects in permutation
+  i = mod289(i);
+  vec3 p =
+      permute(permute(i.y + vec3(0.0, i1.y, 1.0)) + i.x + vec3(0.0, i1.x, 1.0));
+
+  vec3 m = max(0.5 - vec3(dot(x0, x0), dot(x1, x1), dot(x2, x2)), 0.0);
+
+  m = m * m;
+  m = m * m;
+
+  // Gradients:
+  //  41 pts uniformly over a line, mapped onto a diamond
+  //  The ring size 17*17 = 289 is close to a multiple
+  //      of 41 (41*7 = 287)
+
+  vec3 x = 2.0 * fract(p * C.www) - 1.0;
+  vec3 h = abs(x) - 0.5;
+  vec3 ox = floor(x + 0.5);
+  vec3 a0 = x - ox;
+
+  // Normalise gradients implicitly by scaling m
+  // Approximation of: m *= inversesqrt(a0*a0 + h*h);
+  m *= 1.79284291400159 - 0.85373472095314 * (a0 * a0 + h * h);
+
+  // Compute final noise value at P
+  vec3 g = vec3(0.0);
+  g.x = a0.x * x0.x + h.x * x0.y;
+  g.yz = a0.yz * vec2(x1.x, x2.x) + h.yz * vec2(x1.y, x2.y);
+  return 130.0 * dot(m, g);
+}
+
+float fBM(vec2 uv) {
+
+  // Properties
+  const int octaves = 4;
+  float lacunarity = 2.0;
+  float gain = 0.5;
+  //
+  // Initial values
+  float amplitude = 0.5;
+  float frequency = 1.;
+  float y = 0.;
+
+  // Loop of octaves
+  for (int i = 0; i < octaves; i++) {
+    y += amplitude * snoise(frequency * uv);
+    frequency *= lacunarity;
+    amplitude *= gain;
+  }
+
+  return y;
+}
+
 Mesh scene(vec3 point) {
 
   float dist = sin(T) * .5 + .5 + .1;
@@ -188,6 +279,8 @@ Mesh scene(vec3 point) {
   // Mesh sphere2 = Mesh(sphereSdf(point, vec3(.5, 0., 0.), .3), gold());
 
   Mesh plane = Mesh(planeSdf(point, vec3(0., 1., 0.), 1.), checkerboard(point));
+
+  plane.sdf += fBM(point.xz);
 
   const int MESH_NUMB = 2;
 
@@ -213,7 +306,7 @@ Mesh rayMarch(Ray ray) {
 
   float marched = 0.;
   float dist_scene = 0.;
-  float steps = 0.;
+  int steps = 0;
 
   Mesh closest_object = Mesh(MAX_DEPTH, silver());
 
@@ -228,7 +321,7 @@ Mesh rayMarch(Ray ray) {
     if (dist_scene < PRECISION || marched > MAX_DEPTH)
       break;
 
-    steps += 1.;
+    steps++;
   }
 
   closest_object.sdf = marched;
@@ -241,7 +334,7 @@ Mesh rayMarch(Ray ray) {
 
 vec3 getNormal(vec3 point) {
 
-  const float EPSILON = 0.001;
+  const float EPSILON = 0.01;
 
   vec2 e = vec2(EPSILON, 0.0);
   vec3 n = vec3(scene(point).sdf) - vec3(scene(point - e.xyy).sdf,
