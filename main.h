c8b9989656abb117b6f94f102c05d9307464f5bf
Toma Adrian <sirhadrian21@gmail.com>
2023-10-16 15:40:34 +0300

    header update

--- main.h
+++ main.h
@@ -1,12 +1,9 @@
 #pragma once
-// clang-format off
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-// clang-format on
 
 #include <math.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 #ifndef TRUE
 #define TRUE 1
@@ -14,6 +11,14 @@
 
 #ifndef FALSE
 #define FALSE 0
+#endif
+
+#ifndef EXIT_SUCCESS
+#define EXIT_SUCCESS 0
+#endif
+
+#ifndef EXIT_FAILURE
+#define EXIT_FAILURE -1
 #endif
 
 #define WIDTH 1920.0f
@@ -32,10 +37,9 @@
 #define MINOR_VERS 6
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
-static void cursor_position_callback(GLFWwindow *window, double xPos,
-                                     double yPos);
+static void cursor_position_callback(GLFWwindow *window, double xPos, double yPos);
 void cursor_enter_callback(GLFWwindow *window, int inside);
 void process_input(GLFWwindow *window, GLuint *shader_program);
-char *get_shader(char *shader_file);
+char* get_shader(char *shader_file);
 void die(const char *error);
 void compile_shaders(const GLuint *const shader_program);
