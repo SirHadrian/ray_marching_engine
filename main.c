4db3396b44be0e97d1d61077179ace6c0998fcd4
Toma Adrian Ionut <sirhadrian21@gmail.com>
2024-04-24 23:35:07 +0300

    format

--- main.c
+++ main.c
@@ -1,265 +1,257 @@
 #include "main.h"
-#include <stdlib.h>
 
 // Cursor state
-double xMousePos, yMousePos = 0.f;
+double xMousePos = 0.f, yMousePos = 0.f;
 int inWindow = FALSE;
 
-int 
-main(void) 
+int
+main(void)
 {
-  if (!glfwInit())
-    die("Could not initialize GLFW");
+        if (!glfwInit()) {
+                die("Could not initialize GLFW");
+        }
 
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, MAJOR_VERS);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, MINOR_VERS);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, MAJOR_VERS);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, MINOR_VERS);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, TITLE, NULL, NULL);
+        GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, TITLE, NULL, NULL);
 
-  if (!window) 
-  {
-    glfwTerminate();
-    die("Failed to create GLFW window");
-  }
+        if (!window) {
+                glfwTerminate();
+                die("Failed to create GLFW window");
+        }
 
-  glfwMakeContextCurrent(window);
+        glfwMakeContextCurrent(window);
 
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    die("Failed to initialize GLAD");
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+                die("Failed to initialize GLAD");
+        }
 
-  glViewport(0, 0, WIDTH, HEIGHT);
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+        glViewport(0, 0, WIDTH, HEIGHT);
+        glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-  // Mouse
-  glfwSetCursorPosCallback(window, cursor_position_callback);
-  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-  glfwSetCursorEnterCallback(window, cursor_enter_callback);
+        // Mouse
+        glfwSetCursorPosCallback(window, cursor_position_callback);
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+        glfwSetCursorEnterCallback(window, cursor_enter_callback);
 
-  float vertices[] = {
-    1.0f,  1.0f,  0.0f, // top right
-    1.0f,  -1.0f, 0.0f, // bottom right
-    -1.0f, -1.0f, 0.0f, // bottom left
-    -1.0f, 1.0f,  0.0f  // top left
-  };
+        float vertices[] = {
+                1.0f,  1.0f,  0.0f, // top right
+                1.0f,  -1.0f, 0.0f, // bottom right
+                -1.0f, -1.0f, 0.0f, // bottom left
+                -1.0f, 1.0f,  0.0f  // top left
+        };
 
-  uint indices[] = {
-    // note that we start from 0!
-    0, 1, 3, // first triangle
-    1, 2, 3  // second triangle
-  };
+        uint indices[] = {
+                // note that we start from 0!
+                0, 1, 3, // first triangle
+                1, 2, 3  // second triangle
+        };
 
-  GLuint VAO, VBO, EBO;
+        GLuint VAO, VBO, EBO;
 
-  glGenVertexArrays(1, &VAO);
-  glGenBuffers(1, &VBO);
-  glGenBuffers(1, &EBO);
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
 
-  glBindVertexArray(VAO);
+        glBindVertexArray(VAO);
 
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-      GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+                     GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-  glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+        glEnableVertexAttribArray(0);
 
-  // glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
-  //                       (void *)(3 * sizeof(float)));
-  // glEnableVertexAttribArray(1);
+        // glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+        //                       (void *)(3 * sizeof(float)));
+        // glEnableVertexAttribArray(1);
 
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-  glBindVertexArray(0);
+        glBindVertexArray(0);
 
-  // Shader program
-  GLuint shader_program = glCreateProgram();
-  compile_shaders(&shader_program);
+        // Shader program
+        GLuint shader_program = glCreateProgram();
+        compile_shaders(&shader_program);
 
-  // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-  // Render loop
-  while (!glfwWindowShouldClose(window)) 
-  {
-    // Input
-    process_input(window, &shader_program);
+        // Render loop
+        while (!glfwWindowShouldClose(window)) {
+                // Input
+                process_input(window, &shader_program);
 
-    // Render
-    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+                // Render
+                glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+                glClear(GL_COLOR_BUFFER_BIT);
 
-    float time = glfwGetTime();
-    GLuint u_time_location = glGetUniformLocation(shader_program, UNIFORM_TIME);
-    GLuint u_resolution_location =
-      glGetUniformLocation(shader_program, UNIFORM_RESOLUTION);
-    GLuint u_mouse_location =
-      glGetUniformLocation(shader_program, UNIFORM_MOUSE);
+                float time = glfwGetTime();
+                GLuint u_time_location = glGetUniformLocation(shader_program, UNIFORM_TIME);
+                GLuint u_resolution_location =
+                        glGetUniformLocation(shader_program, UNIFORM_RESOLUTION);
+                GLuint u_mouse_location =
+                        glGetUniformLocation(shader_program, UNIFORM_MOUSE);
 
-    glUseProgram(shader_program);
-    glUniform1f(u_time_location, time);
-    glUniform2f(u_resolution_location, WIDTH, HEIGHT);
-    glUniform2f(u_mouse_location, xMousePos, yMousePos);
+                glUseProgram(shader_program);
+                glUniform1f(u_time_location, time);
+                glUniform2f(u_resolution_location, WIDTH, HEIGHT);
+                glUniform2f(u_mouse_location, xMousePos, yMousePos);
 
-    glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    // glDrawArrays(GL_TRIANGLES, 0, 3);
-    // glBindVertexArray(0);
+                glBindVertexArray(VAO);
+                glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+                // glDrawArrays(GL_TRIANGLES, 0, 3);
+                // glBindVertexArray(0);
 
-    // Swap buffers and pull IO events
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
+                // Swap buffers and pull IO events
+                glfwSwapBuffers(window);
+                glfwPollEvents();
+        }
 
-  // Dealocate resources
-  glDeleteVertexArrays(1, &VAO);
-  glDeleteBuffers(1, &VBO);
-  glDeleteBuffers(1, &EBO);
+        // Dealocate resources
+        glDeleteVertexArrays(1, &VAO);
+        glDeleteBuffers(1, &VBO);
+        glDeleteBuffers(1, &EBO);
 
-  glDeleteProgram(shader_program);
+        glDeleteProgram(shader_program);
 
-  glfwTerminate();
+        glfwTerminate();
 
-  return EXIT_SUCCESS;
+        return EXIT_SUCCESS;
 }
 
-void 
-framebuffer_size_callback(GLFWwindow *window, int width, int height) 
+void
+framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-  glViewport(0, 0, width, height);
+        glViewport(0, 0, width, height);
 }
 
-void 
-process_input(GLFWwindow *window, GLuint *shader_program) 
+void
+process_input(GLFWwindow* window, GLuint* shader_program)
 {
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) ||
-      glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
-    glfwSetWindowShouldClose(window, TRUE);
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) || glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS) {
+                glfwSetWindowShouldClose(window, TRUE);
+        } else if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) {
+                glDeleteProgram(*shader_program);
+                *shader_program = glCreateProgram();
 
-  else if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) 
-  {
-    glDeleteProgram(*shader_program);
-    *shader_program = glCreateProgram();
-
-    compile_shaders(shader_program);
-  }
+                compile_shaders(shader_program);
+        }
 }
 
 char*
-get_shader(char *shader_file)
+get_shader(char* shader_file)
 {
-  FILE *file = fopen(shader_file, "r");
-  if (!file)
-  {
-    fprintf(stderr, "ERROR: Could not open file: %s, does it exist?\n", shader_file);
-    exit(EXIT_FAILURE);
-  }
+        FILE *file = fopen(shader_file, "r");
+        if (!file) {
+                fprintf(stderr, "ERROR: Could not open file: %s, does it exist?\n", shader_file);
+                exit(EXIT_FAILURE);
+        }
 
-  fseek(file, 0, SEEK_END);
-  ulint length = (ulint)ftell(file);
-  fseek(file, 0, SEEK_SET);
+        fseek(file, 0, SEEK_END);
+        ulint length = (ulint)ftell(file);
+        fseek(file, 0, SEEK_SET);
 
-  char *shader_string = malloc(sizeof *shader_string * (length + 1));
-  if (!shader_string) 
-    die("Could not alocate memory for the shader file contents");
+        char* shader_string = malloc(sizeof(*shader_string) * (length + 1));
+        if (!shader_string) {
+                die("Could not alocate memory for the shader file contents");
+        }
 
-  int cursor;
-  uint index = 0;
+        int cursor;
+        uint index = 0;
 
-  while ((cursor = fgetc(file)) != EOF) 
-  {
-    shader_string[index] = (char)cursor;
-    index++;
-  }
-  shader_string[length] = '\0';
+        while ((cursor = fgetc(file)) != EOF) {
+                shader_string[index] = (char)cursor;
+                index++;
+        }
+        shader_string[length] = '\0';
 
-  fclose(file);
+        fclose(file);
 
-  return shader_string;
+        return shader_string;
 }
 
-void 
-die(const char *error) 
+void
+die(char const* error)
 {
-  fprintf(stderr, "ERROR: %s\n", error);
-  exit(EXIT_FAILURE);
+        fprintf(stderr, "ERROR: %s\n", error);
+        exit(EXIT_FAILURE);
 }
 
-void 
-compile_shaders(const GLuint *const shader_program) 
+void
+compile_shaders(GLuint const* const shader_program)
 {
-  char *vertex_shader_source = get_shader(VERTEX_SHADER_PATH);
+        char* vertex_shader_source = get_shader(VERTEX_SHADER_PATH);
 
-  GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vertex_shader, 1, (char const *const *)&vertex_shader_source,
-      NULL);
-  glCompileShader(vertex_shader);
+        GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertex_shader, 1, (char const *const *)&vertex_shader_source,
+                       NULL);
+        glCompileShader(vertex_shader);
 
-  int success;
-  char info_log[512];
-  glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+        int success;
+        char info_log[512];
+        glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
 
-  if (!success) 
-  {
-    glGetShaderInfoLog(vertex_shader, 512, NULL, info_log);
-    fprintf(stderr, "Vertex shader compilation error: %s\n", info_log);
-    return;
-  }
+        if (!success) {
+                glGetShaderInfoLog(vertex_shader, 512, NULL, info_log);
+                fprintf(stderr, "Vertex shader compilation error: %s\n", info_log);
+                return;
+        }
 
-  free(vertex_shader_source);
+        free(vertex_shader_source);
 
-  char *fragment_shader_source = get_shader(FRAGMENT_SHADER_PATH);
+        char* fragment_shader_source = get_shader(FRAGMENT_SHADER_PATH);
 
-  GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragment_shader, 1,
-      (char const *const *)&fragment_shader_source, NULL);
-  glCompileShader(fragment_shader);
+        GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragment_shader, 1,
+                       (char const *const *)&fragment_shader_source, NULL);
+        glCompileShader(fragment_shader);
 
-  glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+        glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
 
-  if (!success) 
-  {
-    glGetShaderInfoLog(fragment_shader, 512, NULL, info_log);
-    fprintf(stderr, "Fragment shader compilation error: %s\n", info_log);
-    return;
-  }
+        if (!success) {
+                glGetShaderInfoLog(fragment_shader, 512, NULL, info_log);
+                fprintf(stderr, "Fragment shader compilation error: %s\n", info_log);
+                return;
+        }
 
-  free(fragment_shader_source);
+        free(fragment_shader_source);
 
-  glAttachShader(*shader_program, vertex_shader);
-  glAttachShader(*shader_program, fragment_shader);
-  glLinkProgram(*shader_program);
+        glAttachShader(*shader_program, vertex_shader);
+        glAttachShader(*shader_program, fragment_shader);
+        glLinkProgram(*shader_program);
 
-  glGetProgramiv(*shader_program, GL_LINK_STATUS, &success);
+        glGetProgramiv(*shader_program, GL_LINK_STATUS, &success);
 
-  if (!success) 
-  {
-    glGetProgramInfoLog(*shader_program, 512, NULL, info_log);
-    fprintf(stderr, "Shader program linking error: %s\n", info_log);
-    return;
-  }
+        if (!success) {
+                glGetProgramInfoLog(*shader_program, 512, NULL, info_log);
+                fprintf(stderr, "Shader program linking error: %s\n", info_log);
+                return;
+        }
 
-  glDeleteShader(vertex_shader);
-  glDeleteShader(fragment_shader);
+        glDeleteShader(vertex_shader);
+        glDeleteShader(fragment_shader);
 }
 
-static void 
-cursor_position_callback(GLFWwindow *window, double xPos, double yPos) 
+static void
+cursor_position_callback(GLFWwindow* window, double xPos, double yPos)
 {
-  if (inWindow) 
-  {
-    xMousePos = xPos;
-    yMousePos = yPos;
-  }
+        if (inWindow) {
+                xMousePos = xPos;
+                yMousePos = yPos;
+        }
 }
 
-void 
-cursor_enter_callback(GLFWwindow *window, int inside) 
+void
+cursor_enter_callback(GLFWwindow* window, int inside)
 {
-  if (inside) 
-    inWindow = TRUE;
-  else 
-    inWindow = FALSE;
+        if (inside) {
+                inWindow = TRUE;
+        } else {
+                inWindow = FALSE;
+        }
 }
--- main.h
+++ main.h
@@ -1,8 +1,6 @@
-#pragma once
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -14,37 +12,29 @@
 #define FALSE 0
 #endif
 
-#ifndef EXIT_SUCCESS
-#define EXIT_SUCCESS 0
-#endif
+#define WIDTH   1920.0f
+#define HEIGHT  1080.0f
 
-#ifndef EXIT_FAILURE
-#define EXIT_FAILURE -1
-#endif
+#define TITLE                   "OpenGL Template"
 
-#define WIDTH 1920.0f
-#define HEIGHT 1080.0f
+#define UNIFORM_TIME            "u_time"
+#define UNIFORM_MOUSE           "u_mouse"
+#define UNIFORM_RESOLUTION      "u_resolution"
 
-#define TITLE "OpenGL Template"
-
-#define UNIFORM_TIME "u_time"
-#define UNIFORM_MOUSE "u_mouse"
-#define UNIFORM_RESOLUTION "u_resolution"
-
-#define VERTEX_SHADER_PATH "shaders/vertex_shader.c"
-#define FRAGMENT_SHADER_PATH "shaders/fragment_shader.c"
+#define VERTEX_SHADER_PATH      "shaders/vertex_shader.glsl"
+#define FRAGMENT_SHADER_PATH    "shaders/fragment_shader.glsl"
 
 #define MAJOR_VERS 4
 #define MINOR_VERS 6
 
-typedef unsigned int uint;
-typedef unsigned long int ulint;
-typedef unsigned char uchar;
+typedef unsigned int            uint;
+typedef unsigned long int       ulint;
+typedef unsigned char           uchar;
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height);
-static void cursor_position_callback(GLFWwindow *window, double xPos, double yPos);
-void cursor_enter_callback(GLFWwindow *window, int inside);
-void process_input(GLFWwindow *window, GLuint *shader_program);
-char* get_shader(char *shader_file);
-void die(const char *error);
-void compile_shaders(const GLuint *const shader_program);
+void            framebuffer_size_callback(GLFWwindow* window, int width, int height);
+static void     cursor_position_callback(GLFWwindow* window, double xPos, double yPos);
+void            cursor_enter_callback(GLFWwindow* window, int inside);
+void            process_input(GLFWwindow* window, GLuint* shader_program);
+char*           get_shader(char* shader_file);
+void            die(char const* error);
+void            compile_shaders(GLuint const* const shader_program);
